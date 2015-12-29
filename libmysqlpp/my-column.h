01c1afa911f43669632db56ffff952310b76654a
Dan Goodliffe <dan@randomdan.homeip.net>
2015-12-29 05:06:45 +0000

    Tidy up

--- libmysqlpp/my-column.h
+++ libmysqlpp/my-column.h
@@ -17,23 +17,31 @@ namespace MySQL {
 			long unsigned int length;
 			friend class SelectCommand;
 	};
+
 	class StringColumn : public ColumnBase {
 		public:
 			StringColumn(const char * name, unsigned int field, MYSQL_BIND * b, unsigned int len);
 			~StringColumn();
+
 			void apply(DB::HandleField &) const override;
+
 			char * value;
 			long unsigned int length;
 	};
+
 	class NullColumn : public ColumnBase {
 		public:
 			NullColumn(const char * name, unsigned int field, MYSQL_BIND * b);
+
 			void apply(DB::HandleField &) const override;
 	};
+
 	template <class T, enum_field_types MT> class Column : public ColumnBase {
 		public:
 			Column(const char * name, unsigned int field, MYSQL_BIND * b);
+
 			void apply(DB::HandleField & h) const override;
+
 			T value;
 	};
 }
