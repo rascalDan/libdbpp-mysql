f71898826b957548fe703bba1e2f3864dc56c0f8
Dan Goodliffe <dan@randomdan.homeip.net>
2015-12-29 05:01:53 +0000

    Add missing override attribute

--- libmysqlpp/my-column.h
+++ libmysqlpp/my-column.h
@@ -10,7 +10,7 @@ namespace MySQL {
 		public:
 			ColumnBase(const char * name, unsigned int field);
 
-			bool isNull() const;
+			bool isNull() const override;
 
 		protected:
 			my_bool is_null;
@@ -21,19 +21,19 @@ namespace MySQL {
 		public:
 			StringColumn(const char * name, unsigned int field, MYSQL_BIND * b, unsigned int len);
 			~StringColumn();
-			void apply(DB::HandleField &) const;
+			void apply(DB::HandleField &) const override;
 			char * value;
 			long unsigned int length;
 	};
 	class NullColumn : public ColumnBase {
 		public:
 			NullColumn(const char * name, unsigned int field, MYSQL_BIND * b);
-			void apply(DB::HandleField &) const;
+			void apply(DB::HandleField &) const override;
 	};
 	template <class T, enum_field_types MT> class Column : public ColumnBase {
 		public:
 			Column(const char * name, unsigned int field, MYSQL_BIND * b);
-			void apply(DB::HandleField & h) const;
+			void apply(DB::HandleField & h) const override;
 			T value;
 	};
 }
--- libmysqlpp/my-command.h
+++ libmysqlpp/my-command.h
@@ -12,24 +12,24 @@ namespace MySQL {
 			Command(const Connection *, const std::string & sql);
 			virtual ~Command() = 0;
 
-			void bindParamI(unsigned int, int);
-			void bindParamI(unsigned int, long int);
-			void bindParamI(unsigned int, long long int);
-			void bindParamI(unsigned int, unsigned int);
-			void bindParamI(unsigned int, long unsigned int);
-			void bindParamI(unsigned int, long long unsigned int);
+			void bindParamI(unsigned int, int) override;
+			void bindParamI(unsigned int, long int) override;
+			void bindParamI(unsigned int, long long int) override;
+			void bindParamI(unsigned int, unsigned int) override;
+			void bindParamI(unsigned int, long unsigned int) override;
+			void bindParamI(unsigned int, long long unsigned int) override;
 
-			void bindParamB(unsigned int, bool);
+			void bindParamB(unsigned int, bool) override;
 
-			void bindParamF(unsigned int, double);
-			void bindParamF(unsigned int, float);
+			void bindParamF(unsigned int, double) override;
+			void bindParamF(unsigned int, float) override;
 
-			void bindParamS(unsigned int, const Glib::ustring&);
+			void bindParamS(unsigned int, const Glib::ustring&) override;
 
-			void bindParamT(unsigned int, const boost::posix_time::time_duration &);
-			void bindParamT(unsigned int, const boost::posix_time::ptime &);
+			void bindParamT(unsigned int, const boost::posix_time::time_duration &) override;
+			void bindParamT(unsigned int, const boost::posix_time::ptime &) override;
 
-			void bindNull(unsigned int);
+			void bindNull(unsigned int) override;
 		protected:
 			void bindParams();
 			void * realloc(void * buffer, size_t size);
